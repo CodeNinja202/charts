bbbd3fc794901dd1b7d2cd984b294c819b31d874
Ross <rosskeene196@gmail.com>
2023-06-11 13:13:13 -0500

    charts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+ import RenderLineChart from './components/RenderLineChart'
+function App() {
+  return (
+    <div className="App">
+     <RenderLineChart/>
+    </div>
+  );
+}
+
+export default App;
